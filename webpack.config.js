d0c276958f623868e79b285bacf07fbfc7ca19a5
Rodolfo <rodofriaslugo@gmail.com>
2021-02-06 21:48:11 -0600

    ADD: htaccess and public path in webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = env => {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
+        publicPath: '/'
     },
     resolve: {
         extensions: ['.js', '.jsx'],
