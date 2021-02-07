0df1a81aa35b49236b178d3c9708551fadc42431
Rodolfo <rodofriaslugo@gmail.com>
2021-02-06 22:16:01 -0600

    ADD/REMOVE: htaccess deleted, history and react router added as dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./routes/App";
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-        <App/>,
+    <Router history={history}>
+        <App/>
+    </Router>,        
     document.getElementById('app')
 );
